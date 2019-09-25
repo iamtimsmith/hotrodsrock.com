424aa38df95e70f7c0d08c38b2424c0c1d5ad880
Tim Smith <timsmith@Tims-MacBook-Pro.local>
2019-09-25 10:40:12 -0500

    Removed unncessary code from homepage.

--- page-home.php
+++ page-home.php
@@ -66,34 +66,6 @@ $the_query = new WP_Query($args);
 ?>
           <?php if ($the_query->have_posts()): while ($the_query->have_posts()): $the_query->the_post();?>
           <?php if (get_field('show_date') <= $yesterday) {?>
-
-          <div class="col-md-3">
-            <div class="card" style="width: 18rem;">
-              <img src="..." class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                  card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-
-          <div className="column" key={i}>
-            <div className="card">
-              <div className="card-content">
-                <h3>{feature.title}</h3>
-                <div dangerouslySetInnerHTML={{
-																                        __html: feature.description,
-																                      }} />
-                <a href={feature.link} className="button is-primary">
-                  {feature.button}
-                </a>
-              </div>
-            </div>
-          </div>
-
-
           <tr>
             <td><?php echo get_field('show_date') ?></td>
             <td><?php echo get_field('show_time') ?></td>
